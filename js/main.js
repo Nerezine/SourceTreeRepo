b119ab0c7757947fb2d1156da732569ef71e8f1f
Zosia <zofia.wachocka@gmail.com>
2017-01-10 17:20:15 +0100

    1st commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var weScrolled; /*definicja wydarzenia - przewijania*/
+var LastScrolled = 0; /* ostatnia pozycja po przescrollowaniu wzgledem gory okna (gora okna = 0 na osi Y) */
+var delta = 10; /* roznica miedzy gora okna a LastScrolled */
+var wysokoscNavbaru = $("#naglowekRuchomy").outerHeight(); /*  $() parsuje obiekt do JQuery, pozwalajac wywolac metode z tej biblioteki */
+
+$(window).scroll(function(event){  /*window = obszar widoczny ograniczony wielkoscia ekranu, wyrazany w pikselach */
+  weScrolled = true;
+});
+
+setInterval(function() {
+    if(weScrolled){
+      weDidScroll(); /*nowa metoda jest tu konieczna, bo jesli weScrolled=false, to nic dalej sie nie wykona, a powinno */
+      weScrolled = false;
+    }
+}, 300);
+
+function weDidScroll()
